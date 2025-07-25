9eb963f8144160e6f1e389ca3734ae9a42019049
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-25 10:53:12 -0400

    chore(nix): add flake.nix with devShell
    - python
    - poetry
    - libcxx

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+  };
+
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            libcxx
+            python313
+            python313Packages.poetry-core
+            poetry
+            stdenv.cc.cc
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+          '';
+        };
+      });
+    };
+}
