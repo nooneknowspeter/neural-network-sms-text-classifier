d946e2558580d043db818099d14241960ebb1eb6
nooneknows <157999171+nooneknowspeter@users.noreply.github.com>
2025-07-27 17:13:04 -0400

    chore(nix): add nodejs to default devShell packages

--- flake.nix
+++ flake.nix
@@ -1,22 +1,19 @@
 {
-  inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    };
-  };
+  inputs = { nixpkgs = { url = "github:nixos/nixpkgs/nixpkgs-unstable"; }; };
 
   outputs = inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import inputs.nixpkgs { inherit system; };
-      });
-    in
-    {
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import inputs.nixpkgs { inherit system; }; });
+    in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             libcxx
+            nodejs
             python313
             python313Packages.poetry-core
             poetry
